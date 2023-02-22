f99896b02600786d7226c96ad79ac567ffcc5b3f
emdeutsch <emdeutsch@wisc.edu>
2023-02-22 13:58:03 -0600

    switch comma separator to ~

--- hw2/my_parser.py
+++ hw2/my_parser.py
@@ -89,7 +89,7 @@ def parseJson(json_file):
             item_array = []
             item_array.append(itemID)
             item_array.append(item['Name'].replace('"', ''))
-            item_array.append((','.join(item['Category'])).replace('"', ''))
+            item_array.append(('~'.join(item['Category'])).replace('"', ''))
             item_array.append(transformDollar(item['Currently']))
             item_array.append(transformDollar(item['First_Bid']))
             item_array.append(item['Number_of_Bids'])
